51013d78730c20aaa0449069b125d6448c4e64e9
Tracy Silverio <tiritord@gmail.com>
2023-06-23 22:11:48 -0400

    Reproductor de Musica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'piano';
+  title = 'Music';
+validar:string=""
+  aplicarSonido(nombre:string):void{
+    const audio=new Audio();
+    audio.src='../assets/musica/' + nombre + '.mp3';
+    audio.load();
+    audio.play();
+  }
+
+musica:any[]=[
+{nombre:'Rochy',genero:'dembow'},
+{nombre:'Alfa',genero:'dembow'},
+{nombre:'Anuel',genero:'trap'}
+
+]
+
+//       validar:string="";
+//     verificar ():void{
+
+//  if(this.validar=='juan'){
+// console.log("tu codigo es validado")
+//   }
+
+//       }
+    
+
+ 
+
+
 }
